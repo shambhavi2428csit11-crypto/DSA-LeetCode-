912e9d5f32d4ad44ac8f2176d1d9c2ccf2a7f236
shambhavi2428csit11-crypto <shambhavisingh943@gmail.com>
2026-08-14 18:40:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int func(int n,vector<int>&dp)
-    {
-        if(dp[n]!=-1) return dp[n];
-        if(n<=1)
-        {
-            
-            return dp[n]=1;
-        }
-        
-        return dp[n]=func(n-1,dp)+func(n-2,dp);
+int func(int n, vector<int>&dp){
+    if(dp[n]!=-1){
+        return dp[n];
     }
+if(n<=1){
+    return dp[n]=1;
+}
+return dp[n]=func(n-1,dp) +  func(n-2,dp);}
+
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
+        vector<int> dp(n+1,-1);
+
         return func(n,dp);
+    
+        
     }
 };
