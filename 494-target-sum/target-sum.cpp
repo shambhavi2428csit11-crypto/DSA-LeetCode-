8e442555b8efc62a8babc55b13fa3e069d3516ee
shambhavi2428csit11-crypto <shambhavisingh943@gmail.com>
2026-08-21 18:19:18 +0530

    Time: 24 ms (39.79%) | Memory: 59 MB (8.66%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int solve(int i,vector<int>& nums, int target,vector<vector<int>>&dp){
+    if(i==nums.size()){
+        if(target==0){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }int index = target + 2000;
+    if(dp[i][index]!=-1){
+        return dp[i][index];
+    }
+    int plus=solve(i+1,nums,target+nums[i],dp);
+    int minus=solve(i+1,nums,target-nums[i],dp);
+    return dp[i][index]=plus+minus;
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        vector<vector<int>> dp(nums.size(), vector<int>(4001, -1));
+        return solve(0,nums,target,dp);
+    }
+};
