982f425b7a4c1f409a94a1880fb9a8933444e5d3
shambhavi2428csit11-crypto <shambhavisingh943@gmail.com>
2026-09-06 22:06:14 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (45.06%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-    if(i==triangle.size()-1){
-        return triangle[i][j];
-    }
-    if(dp[i][j] != INT_MAX) {
-            return dp[i][j];
-        }
-    int down=solve(i+1,j,triangle,dp);
-    int side=solve(i+1,j+1,triangle,dp);
-    return dp[i][j]=triangle[i][j]+min(down,side);
+// int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
+//     if(i==triangle.size()-1){
+//         return triangle[i][j];
+//     }
+//     // if(dp[i][j] != INT_MAX) {
+//     //         return dp[i][j];
+//     //     }
+//     // int down=solve(i+1,j,triangle,dp);
+//     // int side=solve(i+1,j+1,triangle,dp);
+//     // return dp[i][j]=triangle[i][j]+min(down,side);
 
 
 
-}
+// }
     int minimumTotal(vector<vector<int>>& triangle) {
-          int n = triangle.size();
+        int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-        return solve(0,0,triangle,dp); 
+        for(int j=0;j<n;j++){
+        dp[n-1][j]=triangle[n-1][j];}
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+               
+                int down=dp[i+1][j];
+                int side=dp[i+1][j+1];
+                dp[i][j]=triangle[i][j]+min(down,side);
+                
+            }
+        }
+        return dp[0][0]; 
         
     }
 };
