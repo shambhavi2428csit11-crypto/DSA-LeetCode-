f9016f68b4a04055d1f88f66a4ac5589df60f284
shambhavi2428csit11-crypto <shambhavisingh943@gmail.com>
2026-09-06 21:43:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (73.47%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,17 +15,19 @@ public:
         // return dp[i][j]=down+right;
     // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        vector<int>next(n+1,0);
         for(int i=m-1;i>=0;i--){
+            vector<int>curr(n+1,0);
             for(int j=n-1;j>=0;j--){
-                if(i==m-1 && j==n-1) dp[i][j]=1;
+                if(i==m-1 && j==n-1) curr[j]=1;
                 else{
-                    int down=dp[i+1][j];
-                    int right=dp[i][j+1];
-                    dp[i][j]=down+right;
+                    int down=next[j];
+                    int right=curr[j+1];
+                    curr[j]=down+right;
                 }
             }  
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
